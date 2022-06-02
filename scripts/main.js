f463149333029f27b26251bd3d58f8fa331fae76
katas8n <kirka6061512@icloud.com>
2022-06-02 21:19:29 +0300

    Added js

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// prompt (function by default)
+
+// variables
+
+// var userName = prompt('Eneter ur name : ');
+
+// userName = 'mike';
+
+// alert (function by default)
+// alert(userName);
+
+// var userName = 'John';
+// var userSurname = 'Doe';
+
+// alert(userName + ' ' + userSurname);
