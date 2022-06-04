4c91cfd99b6f90433395a26904bb3df7c0aabdb3
MaksonGitHub25 <makson94021@gmail.com>
2022-06-04 10:06:54 +0300

    Complete quiz in js

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,21 @@ const quizBtn = document.querySelector('.btn');
 quiz();
 
 function quiz() {
+    const userName = prompt('Как вас зовут?');
     
+    if (userName === 'Maks') {
+        alert('Салам, красавчик');
+    } else if (userName === 'Kir') {
+        alert('ОООО, доброго времени суток');
+    } else {
+        alert(`Здравствуйте, ${userName}`);
+    }
+
+    const userSurname = prompt('Какая у вас фамилия?');
+    const userAge = prompt('Сколько вам лет?');
+
+    if (userAge.includes('23')) {
+        alert('Welcome to the club, body');
+    }
 }
+
