fb8c8be790d375386a41410fae53a909f99f9796
MaksonGitHub25 <makson94021@gmail.com>
2022-06-04 21:25:15 +0300

    Proj with Complete JS check

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,106 @@
 const quizBtn = document.querySelector('.btn');
 
-quiz();
+quizHandler();
 
-function quiz() {
-    const userName = prompt('Как вас зовут?');
+function quizHandler() {
+    checkUserName(askUserName);
+    checkUserSurname(askUserSurname);
+    checkUserAge(askUserAge);
+}
+
+// function quiz(userName) {
+    // askUserName();
+    // console.log(typeof(userName));
+    // console.log(userName);
+
+    // if (typeof(userName) == null || userName == '') {
+    //     alert('Вы не ввели имя');
+    //     askUserName();
+    // }
+    // if (userName === 'Maks') {
+    //     alert('Салам, красавчик');
+    // } else if (userName === 'Kir') {
+    //     alert('ОООО, доброго времени суток');
+    // } else {
+    //     alert(`Здравствуйте, ${userName}`);
+    // }
     
-    if (userName === 'Maks') {
-        alert('Салам, красавчик');
-    } else if (userName === 'Kir') {
-        alert('ОООО, доброго времени суток');
+    // const userSurname = prompt('Какая у вас фамилия?');
+    // const userAge = prompt('Сколько вам лет?');
+    
+    // if (userAge.includes('23')) {
+    //     alert('Welcome to the club, body');
+    // }
+// }
+
+function askUserName() {
+    const userName = prompt('Как вас зовут?');
+    return userName;
+}
+
+function checkUserName(askUserName) {
+    let userName = askUserName();
+    console.log(userName);
+    console.log(typeof(userName));
+
+    if (userName == '') {
+        alert('Вы не ввели имя');
+        checkUserName(askUserName);
     } else {
-        alert(`Здравствуйте, ${userName}`);
-    }
-
-    const userSurname = prompt('Какая у вас фамилия?');
-    const userAge = prompt('Сколько вам лет?');
-
-    if (userAge.includes('23')) {
-        alert('Welcome to the club, body');
+        if (userName === 'Maks') {
+            alert('Салам, красавчик');
+        } else if (userName === 'Kir') {
+            alert('ОООО, доброго времени суток');
+        } else {
+            alert(`Здравствуйте, ${userName}`);
+        }
     }
 }
 
-quizBtn.addEventListener('click', quiz);
+function askUserSurname() {
+    const userSurname = prompt('Какая у вас фамилия?');
+    console.log(userSurname);
+    console.log(typeof(userSurname));
+    return userSurname;
+}
+
+function checkUserSurname(askUserSurname) {
+    let userSurname = askUserSurname();
+
+    if (userSurname == '') {
+        alert('Вы не ввели фамилию. Видимо, вы хотите остаться анонимным)');
+        checkUserSurname(askUserSurname);
+    } else {
+        if (userSurname == 'Gates') {
+            alert('I love u and ur money, Bill ! \u{1F60D}');
+        } else if (userSurname == 'Mask') {
+            alert('I wanna drive on ur big pESLA \u{1F60D}');
+        }
+    }
+}
+
+function askUserAge() {
+    const userAge = prompt('Сколько вам лет?');
+
+    console.log(userAge);
+    console.log(typeof(userAge));
+    console.log(parseInt(userAge));
+
+    return userAge;
+}
+
+function checkUserAge(askUserAge) {
+    let userAge = askUserAge();
+
+    if (/[a-zа-яё]/i.test(userAge) || userAge == '') {
+        alert('Вы ввели не число');
+        checkUserAge(askUserAge);
+    } else {
+        if (userAge.includes('23')) {
+            alert('Welcome to the club, body');
+        }
+    }
+
+}
+
+quizBtn.addEventListener('click', quizHandler);
